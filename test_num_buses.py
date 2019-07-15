15655ce31824ef63890d57752ab445223a3648da
Mateusz <mateuszdyrdol@gmail.com>
2019-07-15 07:19:35 +0200

    small fixes

--- test_num_buses.py
+++ test_num_buses.py
@@ -3,9 +3,32 @@ import unittest
 
 
 class TestNumBuses(unittest.TestCase):
-    """ Test class for function a1.num_buses. """
+    """ Test class for function a1.num_buses. """\
+	
+	def test_numbuses_zero(self):
+        actual = a1.num_buses(0)
+		expected = 0
+        self.assertEqual(actual, expected)
+		
+	def test_numbuses_one(self):
+		actual = a1.num_buses(1)
+        expected = 1
+        self.assertEqual(actual, expected)
+	
+    def test_num_buses_1(self):
+        actual = a1.num_buses(151)
+        expected = 4
+        self.assertEqual(actual, expected)
 
-    # Add your test methods for a1.num_buses here.
+    def test_num_buses_2(self):
+        actual = a1.num_buses(200)
+        expected = 4
+        self.assertEqual(actual, expected)
+
+    def test_num_buses_3(self):
+        actual = a1.num_buses(201)
+        expected = 5
+        self.assertEqual(actual,expected)
 
 
 if __name__ == '__main__':
--- test_stock_price_summary.py
+++ test_stock_price_summary.py
@@ -4,9 +4,40 @@ import unittest
 
 class TestStockPriceSummary(unittest.TestCase):
     """ Test class for function a1.stock_price_summary. """
+	def test_stock_price_summary_empty(self):
+        actual = a1.stock_price_summary([])
+		expected = (0, 0)
+        self.assertEqual(actual, expected)
+			
+	def test_stock_price_summary_onezero(self):
+        expected = (0, 0)
+        actual = a1.stock_price_summary([0.0])
+        self.assertEqual(actual, expected)		
+		
+	def test_stock_price_summary_onepositive(self):
+        actual = a1.stock_price_summary([1.2])
+		expected = (1.2, 0)
+        self.assertEqual(actual, expected)
 
-    # Add your test methods for a1.stock_price_summary here.
+    def test_stock_price_summary_onenegative(self):
+        actual = a1.stock_price_summary([-1.2])
+		expected = (0, -1.2)
+        self.assertEqual(actual, expected)	
+			
+    def test_stock_price_summary_1(self):
+        actual = a1.stock_price_summary([0.01, 0.03, -0.02, -0.14, 0, 0, 0.10, -0.01])
+        expected = (0.14, -0.17)
+        self.assertEqual(actual, expected)
 
+    def test_stock_price_summary_2(self):
+        actual = a1.stock_price_summary([0.10, 0.04, 0.05, 0.52, 0, 0.62, 0.01])
+        expected = (1.34, 0)
+        self.assertEqual(actual, expected)
+
+    def test_stock_price_summary_3(self):
+        actual = a1.stock_price_summary([-0.10, -0.04, -0.05, -0.52, 0, -0.62, -0.01])
+        expected = (0, -1.34)
+        self.assertEqual(actual, expected)
 
 if __name__ == '__main__':
     unittest.main(exit=False)
