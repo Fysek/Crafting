0668e0374c0a036325bb4a95be698caffc4eec11
Mateusz Dyrdół <mateusz.dyrdol@aptiv.com>
2019-07-19 10:39:35 +0200

    rat done

--- a2.py
+++ a2.py
@@ -35,10 +35,60 @@ RAT_2_CHAR = 'P'
 
 class Rat:
     """ A rat caught in a maze. """
-
     # Write your Rat methods here.
-
+	
+	def __init__(self, symbol, row, col):
+	"""(Rat, str, int, int) -> NoneType
+	symbol: the 1-character symbol for the rat
+	row: the row where the rat is located
+	col: the column where the rat is located
+	num_sprouts_eaten: the number of sprouts that this rat has eaten, which is initially 0.
+	"""
+		self.symbol = symbol
+		self.row = row
+		self.col = col
+		self.num_sprouts_eaten = 0
+	
+	def set_location(self, row, col):
+	"""(Rat, int, int) -> NoneType
+	"""
+		self.row = row
+		self.col = col
+	
+	def eat_sprout(self):
+	"""(Rat) -> NoneType
+	"""
+		self.num_sprouts_eaten += 1
+	
+	def __str__(self):
+	"""(Rat) -> str
+	’J at(4,3)ate 2 sprouts.’ no new line
+	"""	
+		s = "{0} at ({1}, {2}) ate {3} sprouts.".format(self.symbol,self.row,self.col,self.num_sprouts_eaten)
+        return s
+	
 class Maze:
     """ A 2D maze. """
 
     # Write your Maze methods here.
+	def __init__(self, ):
+	"""(Maze, list of list of str, Rat, Rat) -> NoneType
+    """
+	
+	def is_wall:
+	"""(Maze, int, int) -> bool
+    """
+	
+	def get_character:
+	"""(Maze, int, int) -> str
+	"""
+	
+	def move:
+	"""(Maze, Rat, int, int) ->bool
+	"""
+	
+	def __str__:
+	"""(Maze) -> str
+	"""
+	
+	
